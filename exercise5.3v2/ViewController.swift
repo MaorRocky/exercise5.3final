a4a2d9470f6701072680bfb0faa05f4011d6e8e6
Maor <maor@365scores.com>
2021-01-10 18:28:20 +0200

    dsd

---
+++ exercise5.3v2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  exercise5.3v2
+//
+//  Created by Scores_Main_User on 1/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+
+}
+
